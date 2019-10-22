5bfe0687fa57a7c12660430b78addbb3d48d0fa9
Dylan Zhao <dylan.zhao@epita.fr>
2019-10-22 17:37:28 +0200

    working app

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import { LinearGradient } from 'expo';
-import { primaryGradientArray } from './utils/Colors';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <LinearGradient colors={primaryGradientArray} style={styles.container}>
-        <StatusBar barStyle="light-content" />;
-        <Text>Open up App.js to start working on your app!</Text>
-      </LinearGradient>
-    );
-  }
+import Main from './app/Main.js';
+
+class App extends React.Component {
+	render() {
+		return <Main />;
+	}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App;
