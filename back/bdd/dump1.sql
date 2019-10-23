fbbd784525f05e1f2de789de9e856a74af9315c9
Gabriel Depotte <depott_g@epita.fr>
2019-10-24 00:17:53 +0200

    [back] Init the database with a simple objcts table

---
+++ back/bdd/dump1.sql
@@ -0,0 +1,13 @@
+CREATE SCHEMA `keepr` ;
+
+CREATE TABLE `keepr`.`registered_object` (
+  `id` INT NOT NULL,
+  `code` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `code_UNIQUE` (`code` ASC) VISIBLE,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+
+INSERT INTO `keepr`.`registered_object` (`id`, `code`, `name`) VALUES ('1', 'test1', 'Marteau');
+INSERT INTO `keepr`.`registered_object` (`id`, `code`, `name`) VALUES ('2', 'test2', 'Tourne-vis');
+INSERT INTO `keepr`.`registered_object` (`id`, `code`, `name`) VALUES ('3', 'test3', 'Perçeuse');
