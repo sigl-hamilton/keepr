8a1f16c7add1fe835e6665ca4085beb7e4c9b544
Gabriel Depotte <depott_g@epita.fr>
2020-01-06 15:13:03 +0100

    [back] Added logs to all user views, deletion fails

--- back/view/UserView.js
+++ back/view/UserView.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const dbInit = require("../model/init");
 const User = require("../model/UserModel")(dbInit.sequelize, dbInit.Sequelize);
+const Log = require("../model/LogModel")(dbInit.sequelize, dbInit.Sequelize);
 
 const jsonParser = bodyParser.json();
 
@@ -10,6 +11,14 @@ function exposeUserViews(app) {
         // Respond all the registeredObjects
         response.type("json");
         User.findAll().then(objects => {
+            // Logging the request
+            Log.create({
+                method: "GET",
+                model: "User",
+                user_id: null,
+                comment: null
+            });
+            // Sending the response
             response.send(JSON.stringify(objects));
         });
     });
@@ -19,6 +28,14 @@ function exposeUserViews(app) {
         response.type("json");
         // Respond the matching registeredObject
         User.findByPk(request.params.id).then(object => {
+            // Logging the request
+            Log.create({
+                method: "GET",
+                model: "User",
+                user_id: request.params.id,
+                comment: request.params.id.toString()
+            });
+            // Sending the response
             response.send(JSON.stringify(object));
         });
     });
@@ -34,6 +51,14 @@ function exposeUserViews(app) {
             createdAt: new Date().toString(),
             updatedAt: new Date().toString()
         }).then(object => {
+            // Logging the request
+            Log.create({
+                method: "POST",
+                model: "User",
+                user_id: object.id,
+                comment: object.id.toString()
+            });
+            // Sending the response
             response.send(JSON.stringify(object));
         }); // FIXME Error management
     });
@@ -46,7 +71,17 @@ function exposeUserViews(app) {
             email: request.body.email,
             type: request.body.type,
             updatedAt: new Date().toString()
-        }, { where: { id: request.params.id }}).then(affectedRowsNb => {
+        }, {
+            where: { id: request.params.id }
+        }).then(affectedRowsNb => {
+            // Logging the request
+            Log.create({
+                method: "PUT",
+                model: "User",
+                user_id: request.params.id,
+                comment: request.params.id
+            });
+            // Sending the response
             response.send(JSON.stringify(affectedRowsNb)); // FIXME Return format, compared to delete
         });
     });
@@ -59,6 +94,14 @@ function exposeUserViews(app) {
                 id: request.params.id
             }
         }).then(affectedRowsNb => {
+            // Logging the request
+            Log.create({
+                method: "DELETE",
+                model: "User",
+                user_id: request.params.id,
+                comment: request.params.id.toString()
+            });
+            // Sending the response
             response.send(JSON.stringify(affectedRowsNb)); // FIXME Return format, compared to update
         });
     });
