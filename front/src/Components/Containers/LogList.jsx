9158254214e5884191f6491274cf66f0ff6e12cf
Gabriel Depotte <depott_g@epita.fr>
2020-01-09 02:18:59 +0100

    [front] Added the LogList component

---
+++ front/src/Components/Containers/LogList.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Table } from 'reactstrap';
+
+export default class LogList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            logs: props.logs,
+            appProps: props.appProps
+        };
+
+        this.buildRows = this.buildRows.bind(this);
+    }
+
+    buildRows() {
+        console.log(this.props);
+        return this.props.logs.map((log, k) =>
+            <tr key={k}>
+                <th>{log.id}</th>
+                <td>{log.method}</td>
+                <td>{log.model}</td>
+                <td>{log.user_id}</td>
+                <td>{log.comment}</td>
+                <td>{log.createdAt}</td>
+            </tr>
+        )
+    }
+
+    render() {
+        return (
+            <Table>
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Method</th>
+                    <th>Model</th>
+                    <th>Concerned user id</th>
+                    <th>Comment</th>
+                    <th>Created at</th>
+                </tr>
+                </thead>
+                <tbody>
+                {this.buildRows()}
+                </tbody>
+            </Table>
+        )
+    }
+}
