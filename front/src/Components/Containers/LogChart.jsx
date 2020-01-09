a59fb0f50cfe12e43743e350f6b7b4defa0fed7c
Gabriel Depotte <depott_g@epita.fr>
2020-01-09 18:28:55 +0100

    [front] Added a No User column

--- front/src/Components/Containers/LogChart.jsx
+++ front/src/Components/Containers/LogChart.jsx
@@ -5,14 +5,6 @@ export default class LogChart extends Component {
     constructor(props) {
         super(props);
 
-        const data = [
-            ["Time", "Sales", "Expenses"],
-            ["2004", 1000, 400],
-            ["2005", 1170, 460],
-            ["2006", 660, 1120],
-            ["2007", 1030, 540]
-        ];
-
         this.state = {
             logs: props.logs,
             appProps: props.appProps
@@ -23,10 +15,10 @@ export default class LogChart extends Component {
 
     buildData() {
         if (this.props.logs === null || this.props.users === null)
-            return [["Time", "All users"]];
+            return [["Time", "All", "No user"]];
         else {
-            let ret = [["Time", "All users"]];
-            let currentAccu = ["", 0];
+            let ret = [["Time", "All", "No user"]];
+            let currentAccu = ["", 0, 0];
 
             this.props.users.forEach((user) => {
                 currentAccu.push(0);
@@ -40,12 +32,14 @@ export default class LogChart extends Component {
                     if (this.props.users[i].id === log.user_id) {
                         found = true;
                         currentAccu[1] += 1;
-                        currentAccu[i+2] += 1
+                        currentAccu[i+3] += 1;
                     }
                 }
                 // If the user has not been found, still add 1 to the sum of all logs
-                if (!found)
+                if (!found) {
                     currentAccu[1] += 1;
+                    currentAccu[2] += 1;
+                }
 
                 // Setting the date of the currentAccu
                 currentAccu[0] = new Date(log.createdAt);
